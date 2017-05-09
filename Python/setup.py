c2b2c30ed469f7f57099b8bc4fdec38f78208642
Ramazan Ozdemir <rozdmr27@gmail.com>
2017-05-09 15:27:12 +0300

    pylint

--- Python/setup.py
+++ Python/setup.py
@@ -1,6 +1,32 @@
 from codecs import open
-from setuptools import setup, find_packages
 
+from Cython.Build import cythonize
+from setuptools import setup, find_packages
+from distutils.core import setup
+from distutils.extension import Extension
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+ext_modules = [ ]
+extensions = [Extension("*", ["*.py"])]
+
+if use_cython:
+    ext_modules += [
+        Extension("khudi", ["khudi/main.py"]),
+        #Extension("khudi/color", ["khudi/color/*.py"]),
+        #Extension("khudi/image", ["khudi/image/*.py"]),
+
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("khudi", [ "khudi/*.c" ]),
+    ]
 
 try:
     long_description = open('README.md').read()
@@ -12,7 +38,9 @@ setup(
     version='0.1.0',
     author='',
     author_email='',
-    packages=find_packages(),
+    packages=[],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
     url='https://github.com/rozdemir/RayTrace/',
     description='',
     long_description=long_description,
